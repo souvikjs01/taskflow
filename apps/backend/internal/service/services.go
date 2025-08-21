388afeff0861e87f6bf536918a3d23edefc5a573
souvikjs01 <souvik741156@gmail.com>
2025-08-21 11:41:46 +0530

    services

--- apps/backend/internal/service/services.go
+++ apps/backend/internal/service/services.go
@@ -1,21 +1,37 @@
 package service
 
 import (
+	"fmt"
+
+	"github.com/souvikjs01/go-tasker/internal/lib/aws"
 	"github.com/souvikjs01/go-tasker/internal/lib/job"
 	"github.com/souvikjs01/go-tasker/internal/repository"
 	"github.com/souvikjs01/go-tasker/internal/server"
 )
 
 type Services struct {
-	Auth *AuthService
-	Job  *job.JobService
+	Auth     *AuthService
+	Job      *job.JobService
+	Todo     *TodoService
+	Comment  *CommentService
+	Category *CategoryService
 }
 
 func NewServices(s *server.Server, repos *repository.Repositories) (*Services, error) {
 	authService := NewAuthService(s)
 
+	s.Job.SetAuthService(authService)
+
+	awsClient, err := aws.NewAWS(s)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AWS client: %w", err)
+	}
+
 	return &Services{
-		Job:  s.Job,
-		Auth: authService,
+		Job:      s.Job,
+		Auth:     authService,
+		Category: NewCategoryService(s, repos.Category),
+		Comment:  NewCommentService(s, repos.Comment, repos.Todo),
+		Todo:     NewTodoService(s, repos.Todo, repos.Category, awsClient),
 	}, nil
 }
